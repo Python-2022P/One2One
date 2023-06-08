cb00fc76d252d667c13d55f874e41e691e178611
abdurasulov-olqar <abdurasulovolqar123@gmail.com>
2023-06-08 16:46:02 +0500

    get group id, GET, PUT, DELETE metods

--- groups/urls.py
+++ groups/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import (
     get_groups,
+    get_group_id,
 )
                      
 urlpatterns = [
-    path('', get_groups),
+   
+    path('<int:id>',get_group_id),
+    # path('', get_groups),
 ]
